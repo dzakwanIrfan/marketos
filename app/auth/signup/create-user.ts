01c2f2635baabbd406416cbdc0a49a640cecaafb
Dzakwan Irfan <lexdani368@gmail.com>
2025-03-28 16:47:39 +0700

    Implement login functionality with error handling and cookie management

--- app/auth/signup/create-user.ts
+++ app/auth/signup/create-user.ts
@@ -1,11 +1,11 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 "use server";
 
+import { FormError } from "@/app/common/form-error.interface";
 import { post } from "@/app/util/fetch";
 import { redirect } from "next/navigation";
 
 export default async function createUser(
-    _prevState: any,
+    _prevState: FormError,
     formData: FormData,
 ) {
     const { error } = await post("users", formData);
--- app/util/fetch.ts
+++ app/util/fetch.ts
@@ -1,10 +1,19 @@
+import { cookies } from "next/headers";
 import { API_URL } from "../constants/api";
 import { getErrorMessage } from "./errors";
 
+const getHeaders = async () => {
+    const c = await cookies();
+    return {
+        Cookie: c.toString(),
+    };
+};
+
 export const post = async (path: string, formData: FormData) => {
+    const headers = await getHeaders();
     const res = await fetch(`${API_URL}/${path}`, {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
+        headers: { "Content-Type": "application/json", ...headers },
         body: JSON.stringify(Object.fromEntries(formData)),
     });
     const parseRes = await res.json();
@@ -13,4 +22,12 @@ export const post = async (path: string, formData: FormData) => {
     }
 
     return { error: "" };
+};
+
+export const get = async (path: string) => {
+    const headers = await getHeaders();
+    const res = await fetch(`${API_URL}/${path}`, {
+        headers: { ...headers },
+    });
+    return res.json();
 };
