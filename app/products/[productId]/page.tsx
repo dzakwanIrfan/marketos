6965afaa575f9fa92571c3759a274eeee27e295e
Dzakwan Irfan <lexdani368@gmail.com>
2025-04-04 22:32:09 +0700

    Refactor product components; implement single product page and API integration

---
+++ app/products/[productId]/page.tsx
@@ -0,0 +1,59 @@
+import { Grid, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+import { API_URL } from "@/app/common/constants/api";
+import getProduct from "./get-product";
+
+interface SingleProductProps {
+  params: {
+    productId: string;
+  };
+}
+
+interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  imageExists: boolean;
+  imageUrl?: string | null;
+}
+
+export default async function SingleProduct({ params }: SingleProductProps) {
+  const product: Product = await getProduct(+params.productId);
+
+  return (
+    <Grid
+      container
+      spacing={3} 
+      sx={{ marginBottom: "2rem" }} 
+      marginTop={{ xs: "1rem", md: "2rem" }}
+    >
+        {/* Image Section */}
+        {product.imageExists && product.imageUrl && (
+        <Grid size={{ xs: 12, md: 6 }}>
+            <Image
+                src={`${API_URL}${product.imageUrl}`} 
+                alt={product.name} 
+                width={0} 
+                height={0} 
+                sizes="100vw" 
+                className="w-full h-auto md:w-3/4" 
+            />
+        </Grid>
+        )}
+
+        {/* Product Details Section */}
+        <Grid size={{ xs: 12, md: 6 }}>
+            <Stack spacing={3} sx={{ padding: { xs: "1rem", md: "0" } }}>
+            <Typography variant="h2" component="h1">
+                {product.name}
+            </Typography>
+            <Typography variant="body1">{product.description}</Typography>
+            <Typography variant="h4" color="primary">
+                ${product.price.toFixed(2)} 
+            </Typography>
+            </Stack>
+        </Grid>
+    </Grid>
+  );
+}
--- app/products/product.tsx
+++ app/products/product.tsx
@@ -1,32 +1,38 @@
-import { Card, Stack, Typography } from "@mui/material";
+"use client";
+
+import { Card, CardActionArea, Stack, Typography } from "@mui/material";
 import { Product as IProduct } from "./interfaces/product.interface";
 import Image from "next/image";
 import { API_URL } from "../common/constants/api";
+import { useRouter } from "next/navigation";
 
 interface ProductProps {
     product: IProduct;
 }
 
 export default function Product({ product }: ProductProps) {
+    const router = useRouter();
     return (
-        <Card className="p-4">
-            <Stack gap={1}>
-                <Typography variant="h5">{product.name}</Typography> 
-                {
-                    product.imageExists && (
-                        <Image 
-                            src={`${API_URL}${product.imageUrl}`}
-                            alt={product.name}
-                            sizes="100vw"
-                            width={0}
-                            height={0}
-                            className="w-full h-auto"
-                        />
-                    )
-                }
-                <Typography>{product.description}</Typography> 
-                <Typography>${product.price}</Typography> 
-            </Stack>
-        </Card>
+        <CardActionArea onClick={() => router.push(`/products/${product.id}`)}>
+            <Card className="p-4">
+                <Stack gap={1}>
+                    <Typography variant="h5">{product.name}</Typography> 
+                    {
+                        product.imageExists && (
+                            <Image 
+                                src={`${API_URL}${product.imageUrl}`}
+                                alt={product.name}
+                                sizes="100vw"
+                                width={0}
+                                height={0}
+                                className="w-full h-auto"
+                            />
+                        )
+                    }
+                    <Typography>{product.description}</Typography> 
+                    <Typography>${product.price}</Typography> 
+                </Stack>
+            </Card>
+        </CardActionArea>
     )
 }
